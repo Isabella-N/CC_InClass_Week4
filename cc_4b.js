33027a57707021bbed83b3ecb99039f84b78447b
Isabella Jones <injones@usf.edu>
2025-09-17 17:46:59 -0400

    initial commit with step 3 being worked on

--- cc_4b.js
+++ cc_4b.js
@@ -1,33 +1,37 @@
 // Shirts, TV, Plates, Shoes, Mugs
 let products = [{
-    sku:"SKU-002", name: "Super Shirts", 
-    category: "apparel", price: 40, 
-    inventory: 200},{
-    sku:"SKU-003", name: "Super TV", 
-    category: "electronics", price: 100, 
-    inventory: 150},{
-    sku:"SKU-004", name: "Super Plates", 
-    category: "kitchen", price: 35, 
-    inventory: 105},{
-    sku:"SKU-005", name: "Super Shoes", 
-    category: "apparel", price: 20, 
-    inventory: 79},{
-    sku:"SKU-006", name: "Super Mugs", 
-    category: "kitchen", price: 15, 
-    inventory: 300}]
+    sku:"SKU-002", name: "Super Shirts", category: "apparel", price: 40.99, inventory: 200},
+    {sku:"SKU-003", name: "Super TV", category: "electronics", price: 100.00, inventory: 150},
+    {sku:"SKU-004", name: "Super Plates", category: "kitchen", price: 35.75, inventory: 105},
+    {sku:"SKU-005", name: "Super Shoes", category: "apparel", price: 20.99, inventory: 79},
+    {sku:"SKU-006", name: "Super Mugs", category: "kitchen", price: 15.00, inventory: 300}]
 
 // Displays all of the discounts for each category
-switch (categoryDiscounts) {
-    case "apparel":
-        console.log("20% off");
-    break;
-    case "electronics":
-        console.log("10% off");
-    break;
-    case "kitchen":
-        console.log("5% off");
-    break;
-    default:
-        console.log("0% off");
-}
+for (let product in products) {
+    let discounts = 0;
 
+    switch (product.category) {
+    case "apparel":
+        discounts = 0.20;
+        break;
+    case "electronics":
+        discounts = 0.10;
+        break;
+    case "kitchen":
+        discounts = 0.05;
+        break;
+    default:
+        discounts = 0;
+}}
+
+let promoPrice = product.price * (product.price*discounts));
+product.promoPrice = promoPrice.toFixed(2)
+
+
+
+//let customerType = ["regular", "student", "senior"]
+
+//if (customerType = "regular") { 
+    //console.log()}
+
+console.log(products);
